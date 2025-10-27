1d9db7893736feeceddc9fa1c8fc1d2c4e191932
kellykky <ketlenk.cunha@gmail.com>
2025-10-27 01:39:34 -0300

    a few changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
     navMenu.classList.toggle('active');
   });
 
-  // Fecha o menu ao clicar em um link
   const navLinks = document.querySelectorAll('.nav-link');
   navLinks.forEach(link => {
     link.addEventListener('click', () => {
@@ -15,4 +14,29 @@ document.addEventListener('DOMContentLoaded', function() {
       navMenu.classList.remove('active');
     });
   });
+
+  const images = [
+    'https://images.pexels.com/photos/15290946/pexels-photo-15290946.jpeg',
+    'https://images.pexels.com/photos/1484657/pexels-photo-1484657.jpeg',
+    'https://cdn.pixabay.com/photo/2025/02/02/17/21/floral-arrangement-9377225_1280.jpg'
+  ];
+
+  const slideshowContainer = document.querySelector('.slideshow');
+  let currentSlide = 0;
+
+  images.forEach((image, index) => {
+    const slide = document.createElement('div');
+    slide.className = `slide ${index === 0 ? 'active' : ''}`;
+    slide.style.backgroundImage = `url('${image}')`;
+    slideshowContainer.appendChild(slide);
+  });
+
+  const slides = document.querySelectorAll('.slide');
+
+  function nextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+  }
+  setInterval(nextSlide, 4000);
 });
